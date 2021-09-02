2268e7f4fd592747c255cb042b5bfcc2a80315a2
finalsummer <642661520de@gmail.com>
2021-09-03 00:17:46 +0800

    add02

--- src/components/MyMenu.vue
+++ src/components/MyMenu.vue
@@ -1,25 +1,23 @@
 <script>
-import {defineComponent } from "vue";
+import { defineComponent } from "vue";
 
 export default defineComponent({
   setup() {
-    return {
-    };
+    return {};
   },
 });
-
 </script>
 
 <template>
-<div>
-<router-link to="/">Go to Home</router-link>
-<router-link to="/01.简单的地图">01.简单的地图</router-link>
-</div>
-
+  <div>
+    <router-link to="/">Go to Home</router-link>
+    <router-link to="/01.AccessibleMap">01.简单的地图</router-link>
+    <router-link to="/02.AdvancedMapboxVectorTiles">02.Mapbox地图</router-link>
+  </div>
 </template>
 
 <style scoped>
-a{
-  display:block;
+a {
+  display: block;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,18 @@
 import *as VueRouter from 'vue-router';
 const routes = [
-  { path: '/', component: () => import('../components/index.vue') },
-  { path: '/01.简单的地图', component: () => import('../components/01.Accessible Map.vue') },
+  { path: '/home', component: () => import('../components/index.vue') },
+  { path: '/01.AccessibleMap', component: () => import('../components/01.Accessible Map.vue') },
+  { path: '/02.AdvancedMapboxVectorTiles', component: () => import('../components/02.Advanced Mapbox Vector Tiles.vue') },
+  { path: '/', redirect: '/home', hidden: true },
 ]
 const router = VueRouter.createRouter({
   history: VueRouter.createWebHashHistory(),
   routes
+})
+router.beforeEach((to, from,next) => {
+  console.log('====================================');
+  console.log(to, from);
+  console.log('====================================');
+  next()
 })
 export default router;
